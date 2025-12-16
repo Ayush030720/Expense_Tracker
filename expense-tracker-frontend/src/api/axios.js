889c1dd5b268e1b5190646a969889b18930bb9a6
Ayush030720 <swanand19ingale@gmail.com>
2025-12-16 16:14:54 +0530

    change the icon

--- expense-tracker-frontend/src/api/axios.js
+++ expense-tracker-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://expense-tracker-liard-six-51.vercel.app/",
 });
 
 API.interceptors.request.use((req) => {
