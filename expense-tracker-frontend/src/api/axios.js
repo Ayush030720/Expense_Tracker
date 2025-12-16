042187cd7e828451ce0a8ba05c92298b2c1244aa
Ayush030720 <swanand19ingale@gmail.com>
2025-12-16 16:24:49 +0530

    URL changes

--- expense-tracker-frontend/src/api/axios.js
+++ expense-tracker-frontend/src/api/axios.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://expense-tracker-liard-six-51.vercel.app/",
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
   return req;
 });
-
 
 export default API;
