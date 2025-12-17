a993d50606a41a8652b3149e72a12cbafda13937
Ayush030720 <swanand19ingale@gmail.com>
2025-12-17 18:05:51 +0530

    atlas connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (err) {
-    console.error(err.message);
+    console.error("MongoDB connection failed:", err.message);
     process.exit(1);
   }
 };
